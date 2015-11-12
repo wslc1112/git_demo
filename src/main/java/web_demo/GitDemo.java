580b7b188a64189aff794037849575ba2bb5d7b4
wslc1112 <395261668@qq.com>
2015-11-12 21:01:50 +0800

    eclipse添加test
    Change-Id: If035f837dcc070e91d93ab1910d463c2476e5954
    Signed-off-by: wslc1112 <395261668@qq.com>

---
+++ src/main/java/web_demo/GitDemo.java
@@ -0,0 +1,9 @@
+package web_demo;
+
+public class GitDemo {
+	String gitDemo="tes";
+	public static void main(String[] args) {
+		GitDemo demo=new GitDemo();
+		System.out.println(demo.gitDemo+2323);
+	}
+}
---
+++ src/main/java/web_demo/GitDemo2.java
@@ -0,0 +1,9 @@
+package web_demo;
+
+public class GitDemo2 {
+	String gitDemo="tes";
+	public static void main(String[] args) {
+		GitDemo2 demo=new GitDemo2();
+		System.out.println(demo.gitDemo+2323);
+	}
+}
